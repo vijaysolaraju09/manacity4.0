c8d9424ebda46bfe38bf94b9e3c51d80ffcf3bd8
vijaysolaraju09 <92515032+vijaysolaraju09@users.noreply.github.com>
2025-07-21 22:18:16 +0530

    Add global theme system

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
 import cartReducer from './slices/cartSlice';
 import productReducer from './slices/productSlice';
+import themeReducer from './slices/themeSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     cart: cartReducer,
     products: productReducer,
+    theme: themeReducer,
   },
 });
 

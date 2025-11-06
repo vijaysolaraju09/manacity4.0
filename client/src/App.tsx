b30edf18c9bf69c8f276901cf68732d3ac9d808e
vijaysolaraju09 <92515032+vijaysolaraju09@users.noreply.github.com>
2025-11-07 00:48:26 +0530

    refactor: restore full app routing entry

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,2 +1,22 @@
-export { AppRoutes } from './app/ManacityApp'
-export { ManacityApp as default } from './app/ManacityApp'
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import { AuthProvider } from '@/auth/AuthProvider';
+import AppRoutes from '@/routes';
+import ThemeProvider from '@/theme/ThemeProvider';
+import { store } from '@/store';
+
+const App = () => (
+  <Provider store={store}>
+    <ThemeProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
+  </Provider>
+);
+
+export { AppRoutes } from '@/routes';
+export default App;
